a44fb86138f31fa685290a2591a901a0e0912c01
KVStack <99619813+KVStack@users.noreply.github.com>
2023-09-19 12:32:27 +0300

    Update and rename main.py to first_app.py

--- first_app.py
+++ first_app.py
@@ -1,13 +1,17 @@
+#import kivy class
 from kivy.app import App
 from kivy.uix.label import Label
 
+#define class to add widgets to it
 class Main(App):
+    #build function adds widgets and other components to the screen
     def build(self):
+        #define the Label class and change its values (font_size, color)
         widget = Label()
         widget.text = 'Hello world!'
         widget.font_size = 50
         widget.color = 0,1,0,1
-
+        
         return widget
 
 Main().run()
